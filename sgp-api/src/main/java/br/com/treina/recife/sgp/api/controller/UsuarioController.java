d7cf8249e41edd53bea9337e198592b6e310e5ed
jeffersonqpm <jefferson.qpm@gmail.com>
2026-03-04 21:04:51 -0300

    Finalizando o UsuarioCOntroller (Delete)

--- sgp-api/src/main/java/br/com/treina/recife/sgp/api/controller/UsuarioController.java
+++ sgp-api/src/main/java/br/com/treina/recife/sgp/api/controller/UsuarioController.java
@@ -10,9 +10,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,15 +45,43 @@ public class UsuarioController {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Usuario> ObterDadosPeloId(@PathVariable Long id) { //@PathVariable a variavel vem no endpoint
+    public ResponseEntity<Usuario> ObterDadosPeloId(@PathVariable Long id) { // @PathVariable a variavel vem no endpoint
         Optional<Usuario> usuario = usuarioService.obterDadosDoUsuario(id);
 
-        if(usuario.isEmpty()){
+        if (usuario.isEmpty()) {
             return ResponseEntity.notFound().build();
 
         }
 
         return ResponseEntity.ok(usuario.get());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> excluir(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioService.obterDadosDoUsuario(id);
+
+        if (usuario.isEmpty()) {
+            return ResponseEntity.notFound().build();
+
+        }
+
+        usuarioService.excluirUsuario(id);
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Usuario> atualizar(@PathVariable Long id, @RequestBody Usuario dados) {
+        Optional<Usuario> usuario = usuarioService.obterDadosDoUsuario(id);
+
+        if (usuario.isEmpty()) {
+            return ResponseEntity.notFound().build();
+
+        }
+
+        return ResponseEntity.ok(usuarioService.atualizarUsuario(id, dados));
+
     }
 
 }
