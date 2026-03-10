4f437b4f9bb2269f3ef70d00c85bd0122aa060bb
jeffersonqpm <jefferson.qpm@gmail.com>
2026-03-09 21:37:47 -0300

    UsuarioRepository - iniciando pesquisa

--- sgp-api/src/main/java/br/com/treina/recife/sgp/api/repository/UsuarioRepository.java
+++ sgp-api/src/main/java/br/com/treina/recife/sgp/api/repository/UsuarioRepository.java
@@ -2,13 +2,20 @@
 
 package br.com.treina.recife.sgp.api.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.treina.recife.sgp.api.model.Usuario;
 
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
+    Optional<Usuario> findByEmail(String email);
+
+    List<Usuario> findByStatus(String status);
     
 }
